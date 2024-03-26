1049ae4b446cf57e1b37e921a32143a1208da5ac
JpCortezF <juanpactz@gmail.com>
2024-03-26 17:13:28 -0300

    Se ha agregado la capacidad de agregar un nuevo usuario utilizando Firebase. Se ha implementado la función  para mostrar un modal con los campos de entrada necesarios para crear un nuevo usuario. Además, se ha añadido la lógica para validar los campos de entrada y enviar la solicitud POST al servidor para crear el usuario. Se ha mejorado la experiencia del usuario al mostrar mensajes de alerta y actualizar la tabla de usuarios después de crear un nuevo usuario.

--- SistemaVenta.AplicacionWeb/wwwroot/js/vistas/Usuario_Index.js
+++ SistemaVenta.AplicacionWeb/wwwroot/js/vistas/Usuario_Index.js
@@ -11,6 +11,22 @@
 let tablaData;
 
 $(document).ready(function () {
+
+    fetch("/Usuario/ListaRoles")
+        .then(response => {
+            return response.ok ? response.json() : Promise.reject(response);
+        })
+        .then(responseJson => {
+            if (responseJson.length > 0) {
+                responseJson.forEach((item) => {
+                    $("#cboRol").append(
+                        $("<option>").val(item.idRol).text(item.descripcion)
+                    )
+                })
+            }
+        })
+
+
     tablaData = $('#tbdata').DataTable({  // se convierte #tbdata a DataTable
         responsive: true,
         "ajax": {            // ajax devuelve toda la data el usuario
@@ -54,12 +70,83 @@ $(document).ready(function () {
                 title: '',
                 filename: 'Reporte Usuarios',
                 exportOptions: {
-                    columns: [2, 3, 4, 5, 6]
+                    columns: [2, 3, 4, 5, 6]  // columnas que se van a exportar en el archivo excel
                 }
             }, 'pageLength'
         ],
-        language: {  // url externa que envia información convertida en español
+        language: {  // url externa que envia la datatable convertida en español
             url: "https://cdn.datatables.net/plug-ins/1.11.5/i18n/es-ES.json"
         },
     });
+})
+
+function mostrarModal(modelo = MODELO_BASE)
+{
+    $("#txtId").val(modelo.idUsuario)
+    $("#txtNombre").val(modelo.nombre)
+    $("#txtCorreo").val(modelo.correo)
+    $("#txtTelefono").val(modelo.telefono)
+    $("#cboRol").val(modelo.idRol == 0 ? $("cboRol option:first").val() : modelo.idRol)
+    $("#cboEstado").val(modelo.esActivo)
+    $("#txtFoto").val("")
+    $("#imgUsuario").attr("src", modelo.urlFoto)
+
+    $("#modalData").modal("show")
+}
+
+$("#btnNuevo").click(function () {
+    mostrarModal()
+})
+
+$("#btnGuardar").click(function () {
+    /*debugger;*/
+
+    const inputs = $("input.input-validar").serializeArray();
+    console.log(inputs); // Muestra todos los inputs serializados en la consola
+
+    const inputsSinValor = inputs.filter((item) => item.value.trim() == "");
+
+    if (inputsSinValor.length > 0) {
+        const mensaje = `Debe completar el campo: "${inputsSinValor[0].name}"`
+        toastr.warning("", mensaje)
+        $(`input[name]="${inputsSinValor[0].name}"`).focus()
+        return;
+    }
+
+    const modelo = structuredClone(MODELO_BASE);
+    modelo["idUsuario"] = parseInt($("#txtId").val())
+    modelo["nombre"] = ($("#txtNombre").val())
+    modelo["correo"] = ($("#txtCorreo").val())
+    modelo["telefono"] = ($("#txtTelefono").val())
+    modelo["idRol"] = ($("#cboRol").val())
+    modelo["esActivo"] = ($("#cboEstado").val())
+
+    const inputFoto = document.getElementById("txtFoto")
+
+    const formData = new FormData();
+    // nombres del UsuarioController Crear(foto, modelo)
+    formData.append("foto", inputFoto.files[0])
+    formData.append("modelo", JSON.stringify(modelo)) // conversión a cadena de texto con stringify
+
+    $("#modalData").find("div.modal-content").LoadingOverlay("show");
+
+    if (modelo.idUsuario == 0) {
+        fetch("Usuario/Crear", {
+            method: "POST",
+            body: formData
+        })
+        .then(response => {
+            $("#modalData").find("div.modal-content").LoadingOverlay("hide");
+            return response.ok ? response.json() : Promise.reject(response);
+        })
+            .then(responseJson => {
+                if (responseJson.estado) { // propiedad de gResponse en el Controller 
+                    tablaData.row.add(responseJson.objeto).draw(false)
+                    $("#modalData").modal("hide")
+                    swal("Listo!", "El usuario fue creado", "success")
+                } else {
+                    swal("Lo sentimos", responseJson.mensaje, "error")
+                }
+            })
+    }
 })
