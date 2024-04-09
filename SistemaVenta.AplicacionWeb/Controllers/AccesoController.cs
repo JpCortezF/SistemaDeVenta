cbbc654501c3ce2d676986b854d872a9eb421d0b
JpCortezF <juanpactz@gmail.com>
2024-04-09 13:41:08 -0300

    Fue creado AccesoController. Se inicia el programa desde la vista Login.

---
+++ SistemaVenta.AplicacionWeb/Controllers/AccesoController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaVenta.AplicacionWeb.Models.ViewModels;
+using SistemaVenta.BBL.Interfaces;
+using SistemaVenta.Entity;
+
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+
+namespace SistemaVenta.AplicacionWeb.Controllers
+{
+    public class AccesoController : Controller
+    {
+        private readonly IUsuarioServices _usuarioServices;
+
+        public AccesoController(IUsuarioServices usuarioServices)
+        {
+            _usuarioServices = usuarioServices;
+        }
+        public IActionResult Login()
+        {
+            ClaimsPrincipal claimUser = HttpContext.User;
+
+            if (claimUser.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(VmUsuarioLogin modelo)
+        {
+            Usuario usuarioEncontrado = await _usuarioServices.ObtenerPorCredenciales(modelo.Correo, modelo.Clave);
+
+            if(usuarioEncontrado == null)
+            {
+                ViewData["Mensaje"] = "No se encontraron coincidencias";
+                return View();  
+            }
+            ViewData["Mensaje"] = null;
+
+            List<Claim> claims = new List<Claim>() // Guardamos la información del usuario una vez logeado.
+            {
+                new Claim(ClaimTypes.Name, usuarioEncontrado.Nombre),
+                new Claim(ClaimTypes.NameIdentifier, usuarioEncontrado.IdUsuario.ToString()),
+                new Claim(ClaimTypes.Role, usuarioEncontrado.IdRol.ToString()),
+                new Claim("UrlFoto", usuarioEncontrado.UrlFoto),
+            };
+
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            AuthenticationProperties properties = new AuthenticationProperties()
+            {
+                AllowRefresh = true,
+                IsPersistent = modelo.MantenerSesion
+            };
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), properties);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- SistemaVenta.AplicacionWeb/Program.cs
+++ SistemaVenta.AplicacionWeb/Program.cs
@@ -1,15 +1,29 @@
 using SistemaVenta.AplicacionWeb.Utilidades.AutoMapper;
+
 using SistemaVenta.IOC;
 using SistemaVenta.AplicacionWeb.Utilidades.Extensiones;
+
 using DinkToPdf;
 using DinkToPdf.Contracts;
+
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(option =>
+    {
+        option.LoginPath = "/Acceso/Login";
+        option.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+    });
+
+
 builder.Services.InyectarDependencia(builder.Configuration);
+
 builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
 
 var context = new CustomAssemblyLoadContext();
@@ -27,10 +41,12 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Acceso}/{action=Login}/{id?}");
 
 app.Run();
